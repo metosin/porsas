1160579634ebc107fa23fe658f1b8705e2613d37
Tommi Reiman <tommi@iki.fi>
2019-04-27 21:09:42 +0300

    alpha4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/porsas "0.0.1-alpha3"
+(defproject metosin/porsas "0.0.1-alpha4"
   :description "Spike Lee for Fast DB-access"
   :url "https://github.com/metosin/porsas"
   :deploy-repositories [["releases" :clojars]]
