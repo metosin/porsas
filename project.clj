2f414732615a4a859f05b464480698b9f375b31d
Tommi Reiman <tommi@iki.fi>
2019-07-30 09:30:01 +0300

    0.0.1-alpha13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/porsas "0.0.1-alpha12"
+(defproject metosin/porsas "0.0.1-alpha13"
   :description "Spike Lee for Fast DB-access with Clojure"
   :url "https://github.com/metosin/porsas"
   :deploy-repositories [["releases" :clojars]]
