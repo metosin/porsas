9bd7b36df41281f618b0462396a3a0d2eb0af916
Janos Meszaros <meszaros.jns@gmail.com>
2021-11-26 16:01:28 +0100

    Add some tests

--- test/porsas/async_test.clj
+++ test/porsas/async_test.clj
@@ -1,31 +1,39 @@
 (ns porsas.async-test
-  (:require [porsas.async :as pa]))
+  (:require
+   [clojure.test :as t]
+   [porsas.async :as pa]
+   [promesa.core :as p]
+   [manifold.deferred :as d]))
 
-(def pool
-  (pa/pool
-    {:uri "postgresql://localhost:5432/hello_world"
-     :user "benchmarkdbuser"
-     :password "benchmarkdbpass"}))
+(t/deftest async
+  (let [pool (pa/pool
+              {:uri      "postgresql://localhost:5432/porsas"
+               :user     "user"
+               :password "password"})]
+    (t/is (= "Apple"
+             (-> (pa/query-one pool ["SELECT name from fruit where id=$1" 1])
+                 (pa/then :name)
+                 (deref))))
 
-(-> (pa/query-one pool ["SELECT randomnumber from WORLD where id=$1" 1])
-    (pa/then :randomnumber))
+    (t/is (= "io.reactiverse.pgclient.PgException: relation \"non_existing\" does not exist"
+             (-> (pa/query-one pool ["SELECT * from non_existing where id=$1" 1])
+                 (pa/then :name)
+                 (pa/catch #(-> % .getMessage))
+                 (deref))))
 
-;;
-;; promesa
-;;
+    (t/is (= ["Apple" "Banana" "Peach" "Orange"]
+             (-> (pa/query pool ["SELECT name from fruit"])
+                 (pa/then-async #(mapv :name %))
+                 (deref))))
 
-(require '[promesa.core :as p])
+    (t/testing "promesa"
+      (t/is (= "Banana"
+               (-> (pa/query-one pool ["SELECT name from fruit where id=$1" 2])
+                   (p/chain :name)
+                   (deref)))))
 
-(-> (pa/query-one pool ["SELECT randomnumber from WORLD where id=$1" 1])
-    (p/chain :randomnumber)
-    (deref))
-
-;;
-;; manifold
-;;
-
-(require '[manifold.deferred :as d])
-
-(-> (pa/query-one pool ["SELECT randomnumber from WORLD where id=$1" 1])
-    (d/chain :randomnumber)
-    (deref))
+    (t/testing "manifold"
+      (t/is (= "Peach"
+               (-> (pa/query-one pool ["SELECT name from fruit where id=$1" 3])
+                   (d/chain :name)
+                   (deref)))))))
